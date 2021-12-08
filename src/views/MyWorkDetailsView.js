4e7513b22f6df90b708ca32636d2552a9f6c1d36
Grace Sutherland <rgs1690@gmail.com>
2021-12-08 08:30:15 -0600

    started positioning on details view

--- src/views/MyWorkDetailsView.js
+++ src/views/MyWorkDetailsView.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
+import styled from 'styled-components';
 import { getInspoByMyWorkId } from '../api/data/InspoData';
 import { getSingleMyWork, deleteMyWork } from '../api/data/myWorkData';
 import InspoCard from '../components/InspoCard';
 import getCurrentUsersUid from '../helpers/getCurrentUserUID';
 
+const DetailsStyle = styled.div`
+display: relative;
+.absoluteContainer {
+  position: absolute;
+  top: 5em;
+  left: 60em;
+}
+.flexContainer {
+  display: inline-flex
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
 export default function MyWorkDetailsView() {
   const [myWork, setMyWork] = useState({});
   const [inspos, setInspos] = useState([]);
@@ -27,45 +41,50 @@ export default function MyWorkDetailsView() {
   return (
     <>
       <div>
-        <div>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={myWork.artUrl} />
-            <Card.Body>
-              <Card.Title>{myWork.artTitle}</Card.Title>
-              <Card.Text>
-                {myWork.artMedium}
-                {myWork.artSize}
-              </Card.Text>
-              <Link
-                to={`/myWorkDetails/${myWork.firebaseKey}`}
-                className="btn btn-primary"
-              >
-                View Details
-              </Link>
-              <Link
-                to={`/editArt/${myWork.firebaseKey}`}
-                className="btn btn-info"
-              >
-                Update
-              </Link>
-              <Button
-                type="button"
-                onClick={() => handleClick()}
-                variant="danger"
-              >
-                Delete
-              </Button>
-            </Card.Body>
-          </Card>
-        </div>
-
-        {inspos.map((inspo) => (
-          <InspoCard
-            key={inspo.firebaseKey}
-            inspo={inspo}
-            setInspos={setInspos}
-          />
-        ))}
+        <DetailsStyle>
+          <div>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={myWork.artUrl} />
+              <Card.Body>
+                <Card.Title>{myWork.artTitle}</Card.Title>
+                <Card.Text>
+                  {myWork.artMedium}
+                  {myWork.artSize}
+                </Card.Text>
+                <Link
+                  to={`/myWorkDetails/${myWork.firebaseKey}`}
+                  className="btn btn-primary"
+                >
+                  View Details
+                </Link>
+                <Link
+                  to={`/editArt/${myWork.firebaseKey}`}
+                  className="btn btn-info"
+                >
+                  Update
+                </Link>
+                <Button
+                  type="button"
+                  onClick={() => handleClick()}
+                  variant="danger"
+                >
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="absoluteContainer">
+            <div className="flexContainer">
+              {inspos.map((inspo) => (
+                <InspoCard
+                  key={inspo.firebaseKey}
+                  inspo={inspo}
+                  setInspos={setInspos}
+                />
+              ))}
+            </div>
+          </div>
+        </DetailsStyle>
       </div>
     </>
   );
