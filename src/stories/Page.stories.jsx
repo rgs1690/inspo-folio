7e9de45b13e7245796660977b1792742c596d0e4
Grace Sutherland <rgs1690@gmail.com>
2021-12-07 19:33:26 -0600

    start storybook

---
+++ src/stories/Page.stories.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Page } from './Page';
+import * as HeaderStories from './Header.stories';
+
+export default {
+  title: 'Example/Page',
+  component: Page,
+};
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template = (args) => <Page {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  // More on composing args: https://storybook.js.org/docs/react/writing-stories/args#args-composition
+  ...HeaderStories.LoggedIn.args,
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+};
