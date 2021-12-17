9ade6397571d8b2adc7f08b7fc9541874dc58961
Grace Sutherland <rgs1690@gmail.com>
2021-12-17 16:42:23 -0600

    added styling to login page

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Spinner } from 'react-bootstrap';
 import { signInUser } from '../api/auth';
 
+const SignInStyle = styled.div`
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    margin-top: 2em;
+  }
+`;
 export default function SignIn({ user }) {
   return (
-    <>
-      {user === null ? (
-        <>
-          <Spinner animation="border" variant="primary" />
-          <Spinner animation="border" variant="info" />
-          <Spinner animation="border" variant="primary" />
-          <Spinner animation="border" variant="info" />
-          <Spinner animation="border" variant="primary" />
-        </>
-      ) : (
-        <div className="text-center mt-5">
-          <h1>Welcome! Sign In!</h1>
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={signInUser}
-          >
-            Sign In
-          </button>
-        </div>
-      )}
-    </>
+    <SignInStyle>
+      <>
+        {user === null ? (
+          <>
+            <Spinner animation="border" variant="primary" />
+            <Spinner animation="border" variant="info" />
+            <Spinner animation="border" variant="primary" />
+            <Spinner animation="border" variant="info" />
+            <Spinner animation="border" variant="primary" />
+          </>
+        ) : (
+          <>
+            <img
+              src="https://dozitgaqgnfnfsxezhpg.supabase.in/storage/v1/object/public/capstone/homeImage.png"
+              alt="logo"
+            />
+            <div className="text-center mt-5">
+              <h1>Sign in to get started!</h1>
+              <button type="button" className="btn" onClick={signInUser}>
+                Sign In
+              </button>
+            </div>
+          </>
+        )}
+      </>
+    </SignInStyle>
   );
 }
 SignIn.propTypes = {
